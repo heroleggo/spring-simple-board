46a766a70cfabb677b4cce4ad18cfd6aa0efb5f6
Youngwoong You <asdfg13232@naver.com>
2023-03-14 13:01:39 +0900

    Init

---
+++ src/main/java/com/heroleggo/springsimpleboard/SpringSimpleBoardApplication.java
@@ -0,0 +1,13 @@
+package com.heroleggo.springsimpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleBoardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSimpleBoardApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/heroleggo/springsimpleboard/SpringSimpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroleggo.springsimpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleBoardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

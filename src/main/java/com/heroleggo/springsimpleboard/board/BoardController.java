5e3dceaba7f92d63c74c9f592007636b0490b7a5
Youngwoong You <asdfg13232@naver.com>
2023-03-16 18:54:37 +0900

    Add POST method on board controller
    - add new DTO for creating board

--- src/main/java/com/heroleggo/springsimpleboard/board/BoardController.java
+++ src/main/java/com/heroleggo/springsimpleboard/board/BoardController.java
@@ -1,9 +1,13 @@
 package com.heroleggo.springsimpleboard.board;
 
+import com.heroleggo.springsimpleboard.board.dto.CreateBoardDto;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +19,11 @@ public class BoardController {
   public ResponseEntity<?> getBoardInfo(@PathVariable Integer boardId) {
     String boardInfo = "board ID : " + boardId.toString();
     return ResponseEntity.status(200).body(boardInfo);
+  }
+
+  @PostMapping()
+  public ResponseEntity<?> createBoard(@RequestBody CreateBoardDto body) {
+    System.out.println(body.getBoardName());
+    return ResponseEntity.status(201).body("created");
   }
 }
---
+++ src/main/java/com/heroleggo/springsimpleboard/board/dto/CreateBoardDto.java
@@ -0,0 +1,11 @@
+package com.heroleggo.springsimpleboard.board.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class CreateBoardDto {
+  private String boardName;
+}
